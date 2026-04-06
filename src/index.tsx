47e2af6986d6e46c6866ac090204f47e1a7572ce
Darwin <darwin.a1120@gmail.com>
2026-04-06 16:25:49 -0500

    feat: Agrego analiticas

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react'; // ← agregar
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,5 +12,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
